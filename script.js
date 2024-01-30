a7b11e9bfab184a46a5c35e342ebd1a5d334f2e7
jrme421 <jeromepchallet@gmail.com>
2024-01-31 08:15:00 +0900

    introduction to arrays

--- script.js
+++ script.js
@@ -87,6 +87,7 @@ console.log(yearsUntilRetirement(1986, "jerome"));
 console.log(yearsUntilRetirement(1956, "sam"));
 */
 
+/*
 //Reviewing Functions
 const calcAge = function (birthYear) {
   return 2024 - birthYear;
@@ -111,3 +112,58 @@ const yearsUntilRetirement = function (birthYear, firstName) {
 
 console.log(yearsUntilRetirement(1986, "jerome"));
 console.log(yearsUntilRetirement(1970, "max"));
+*/
+
+/*
+//Introduction to arrays
+//an array is like a big container in which we throw var that we can later change
+//use [] to create an array
+const friends = ["john", "max", "sam"];
+console.log(friends);
+
+//we can also create an array this way
+//new arrayName(var1,var2,var3)
+const y = new Array(1986, 1984, 208, 2020);
+
+console.log(friends[0]); // jerome displayed
+console.log(friends[2]); // sam displayed
+// return length of the array but it's not 0 based
+// so if there are 3 elements, we get 3 not 2
+console.log(friends.length);
+//display the last value of the array
+console.log(friends[friends.length - 1]); // sam displayed
+
+//reassign the value of an array element
+//here we have moidifed the array friends yet it is defined as a const up there
+//only primitive values are immutable but and array is not a primitive value
+friends[2] = "jay";
+console.log(friends);
+//Howeverwe cannot redeclare the entire array
+//friends = ["bob", "alice"]; // illegal statement
+
+//an array can hold different type of values at the same time
+const firstName = "jerome";
+const jerome = [firstName, "challet", 2024 - 1986, "engineer, friends"];
+console.log(jerome); // "jerome", "challet", 37, "engineer", Array[3]
+console.log(jerome.length); //5
+
+//practice
+const calAge = function (birthYear) {
+  return 2024 - birthYear;
+};
+const years = [1990, 1967, 2002, 2010, 2018];
+console.log(calAge(years)); // NaN
+
+//calculate ages for the 1st, 2nd, last element of the array
+const age1 = calAge(years[0]);
+const age2 = calAge(years[1]);
+const age3 = calAge(years[years.length - 1]); //retrieces the last element
+
+//we can put function calls into an array
+const ages = [
+  calAge(years[0]),
+  calAge(years[1]),
+  calAge(years[years.length - 1]),
+];
+console.log(ages);
+*/
